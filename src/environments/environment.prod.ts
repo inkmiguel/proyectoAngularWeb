7c9ee6d498d377d0beb00ee7de5284bf3281e15e
inkmiguel <migferiapad@gmail.com>
2025-06-07 11:31:50 -0600

    Proyecto Inicializado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDV11to9fd5-BKPYHVlWsFh_ewgIL5YijM',
+    authDomain: 'proyectoweb-d01b8.firebaseapp.com',
+    projectId: 'proyectoweb-d01b8',
+    storageBucket: 'proyectoweb-d01b8.firebasestorage.app',
+    messagingSenderId: '349216994866',
+    appId: '1:349216994866:web:0a1d01f14feeaa34fe6e2b',
+    measurementId: 'G-9Z0XFL6V0J',
+  }
+};
